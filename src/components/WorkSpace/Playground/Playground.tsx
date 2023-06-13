cd68c52c1ca53428c3bc2bacf655bbc9ad56315b
nurilacka-admin <nuri@adaptivescale.com>
2023-06-13 23:51:56 +0200

    Codemirror editor implemented

--- src/components/WorkSpace/Playground/Playground.tsx
+++ src/components/WorkSpace/Playground/Playground.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import PreferenceNav from "./PreferenceNav/PreferenceNav";
+import Split from "react-split";
+import CodeMirror from "@uiw/react-codemirror";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { javascript } from "@codemirror/lang-javascript";
 
 type Props = {};
 
 const Playground = (props: Props) => {
   return (
-    <>
+    <div className="fleex flex-col bg-dark-layer-1 relative">
       <PreferenceNav />
-    </>
+      <Split
+        className="h-[calc(100vh-94px)]"
+        direction="vertical"
+        sizes={[60, 40]}
+        minSize={60}
+      >
+        <div className="w-full overflow-auto">
+          <CodeMirror
+            value="conts a = 1;"
+            theme={vscodeDark}
+            extensions={[javascript()]}
+            style={{ fontSize: 16 }}
+          />
+        </div>
+        <div>Test cases</div>
+      </Split>
+    </div>
   );
 };
 
